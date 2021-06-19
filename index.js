a2902bad96044ecf6af4ce5b52233974d5463d7b
Isaac <isaac.businessiq@gmail.com>
2021-06-18 17:01:21 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {}
+    employee.name = 'Isaac', 
+    employee.streetAddress = '21 Narrowway'
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newEmployee = {...employee}
+    newEmployee[name] = streetAddress;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee.name = 'Sam';
+    employee.streetAddress = '12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name, streetAddress) {
+    const newEmployee = {...employee}
+    newEmployee[name] = streetAddress;
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
+
+  
+    
